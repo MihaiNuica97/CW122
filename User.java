3379fddfb7f5d030eac3354ee9b54da3569fbccd
Xplicitr <mihainuica@gmail.com>
2016-05-14 14:45:52 +0100

    Add files via upload

---
+++ User.java
@@ -0,0 +1,33 @@
+
+public class User {
+	protected String giveName;
+	protected String famName;
+	protected String userID;
+	protected String userPwd;
+	
+	public String getGiveName() {
+		return giveName;
+	}
+	public void setGiveName(String giveName) {
+		this.giveName = giveName;
+	}
+	public String getFamName() {
+		return famName;
+	}
+	public void setFamName(String famName) {
+		this.famName = famName;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+}
